10425171d4ad1b342e5b0be0edb22a7ba4c72324
AxelanO7 <jeremia123.jm@gmail.com>
2024-03-30 03:14:53 +0800

    feat (env): styling, add some library, add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { RecoilRoot } from "recoil";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/not_found";
 
@@ -8,25 +7,31 @@ import Signup from "./pages/auth/sign_up";
 import ForgotPassword from "./pages/auth/forgot_password";
 import CheckEmail from "./pages/auth/check_email";
 import ResetPassword from "./pages/auth/reset_password";
+import HomePage from "./pages/user/home-page";
+import Organization from "./pages/user/organization";
+import DetailEvent from "./pages/user/detail-event";
 
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Signin />} />
-          {/* <Route path="/" element={<Login />} /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Signin />} />
+        {/* <Route path="/" element={<Login />} /> */}
 
-          {/* auth */}
-          <Route path="/login" element={<Signin />} />
-          <Route path="/register" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/check-email" element={<CheckEmail />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+        {/* auth */}
+        <Route path="/login" element={<Signin />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/check-email" element={<CheckEmail />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* user */}
+        <Route path="/home-page" element={<HomePage />} />
+        <Route path="/detail-event" element={<DetailEvent />} />
+        <Route path="/organization" element={<Organization />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,92 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        danger: "#FC2947",
+        success: "#0FBD9D",
+        poppy: {
+          50: "#fdf3f3",
+          100: "#fce4e4",
+          200: "#facece",
+          300: "#f6abab",
+          400: "#ef7a7a",
+          500: "#e35050",
+          600: "#cf3333",
+          700: "#bc2a2a",
+          800: "#902424",
+          900: "#782424",
+          950: "#410e0e",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
