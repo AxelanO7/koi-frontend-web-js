e180133d096de570ba8129dbc09f87dac715e080
AxelanO7 <jeremia123.jm@gmail.com>
2024-04-20 18:56:02 +0800

    Refactor routing paths in App.tsx and update profile page layout in profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ import Signin from "./pages/auth/sign_in";
 import Signup from "./pages/auth/sign_up";
 import ForgotPassword from "./pages/auth/forgot_password";
 import CheckEmail from "./pages/auth/check_email";
-import ResetPassword from "./pages/auth/reset_password";
+import ResetPasswordAuth from "./pages/auth/reset_password";
 import HomePage from "./pages/user/home-page";
 import Organization from "./pages/user/organization";
 import DetailEvent from "./pages/user/detail-event";
 import EventPage from "./pages/user/register";
 import ProfilePage from "./pages/user/profile/profile";
 import EditProfilePage from "./pages/user/profile/edit-profile";
+import ResetPasswordProfilePage from "./pages/user/profile/forgot_password";
 
 function App() {
   return (
@@ -27,7 +28,7 @@ function App() {
         <Route path="/register" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/check-email" element={<CheckEmail />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/reset-password" element={<ResetPasswordAuth />} />
 
         {/* user */}
         <Route path="/home-page" element={<HomePage />} />
@@ -40,6 +41,10 @@ function App() {
         {/* profile */}
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/edit-profile" element={<EditProfilePage />} />
+        <Route
+          path="/profile/reset-password"
+          element={<ResetPasswordProfilePage />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/user/profile/profile.tsx
+++ src/pages/user/profile/profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
   return (
     <>
       <BaseLayout>
-        <div className="flex mx-4 space-x-12 ps-2 pe-4">
+        <div className="flex mx-4 space-x-8 ps-2 pe-4">
           <SidebarSection />
           <DetailProfileSection />
         </div>
